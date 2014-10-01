cb07cb2be6c63a06d6f2b27a7602b06df5749455
cella <hysdisk@macbook.local>
2014-10-01 22:41:33 +0900

    edit 3 box

--- box3d/src/ofApp.cpp
+++ box3d/src/ofApp.cpp
@@ -1,17 +1,34 @@
 #include "ofApp.h"
 
 ofEasyCam cam;
+ofLight light;
 
 int deg;
+int rr;
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    // 画面設定
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofBackground(31);
+    // 照明とカメラ
+    ofEnableLighting();
+    light.setPosition(200, 300, 50);
+    light.enable();
+    cam.setOrientation(ofPoint(-20, 0, 0));
+    // GL設定
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_NORMALIZE);
+    
+    deg = 0;
+    rr = 300;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
-    deg++ % 360;
+    deg = (deg + 1) % 360;
+//    rr = 200 * abs(cos(ofDegToRad(deg))) + 150;
 }
 
 //--------------------------------------------------------------
@@ -24,28 +41,47 @@ void ofApp::draw(){
     float y;
     float z;
 
+    ofSetColor(ofColor::white);
+    ofDrawBitmapString(ofToString(deg), 10,10);
+    ofDrawBitmapString(ofToString(rr), 10,20);
     ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
 
-    box = ofMesh::box(30, 30, 30,1,1,1);
+    box = ofMesh::box(10, 10, 10,1,1,1);
+    sphere = ofMesh::sphere(100);
 
     cam.begin();
+
+    sphere.draw();
+
+    ofRotateY(deg);
     
-    for (int i = 0; i < 36; i++) {
-        x = 300 * cos(ofDegToRad(i*10));
-        y = 0;
-        z = 300 * sin(ofDegToRad(i*10));
-        
+    
+    for (int i = 0; i < 72; i++) {
         ofPushMatrix();
-        ofTranslate(x,y,z);
+        ofTranslate(rr * cos(ofDegToRad(i*5)),0,rr * sin(ofDegToRad(i*5)));
         box.drawWireframe();
         ofPopMatrix();
-        
     }
-        ofRotateY(deg);
+
+    ofRotateX(deg);
+    for (int i = 0; i < 72; i++) {
+        ofPushMatrix();
+        ofTranslate(0,rr * cos(ofDegToRad(i*5)),rr * sin(ofDegToRad(i*5)));
+        box.drawWireframe();
+        ofPopMatrix();
+    }
+
+    ofRotateZ(deg);
+    for (int i = 0; i < 72; i++) {
+        ofPushMatrix();
+        ofTranslate(rr * cos(ofDegToRad(i*5)),rr * sin(ofDegToRad(i*5)),0);
+        box.drawWireframe();
+        ofPopMatrix();
+    }
+
+    cam.end();
     
 
-    
-    cam.end();
     
 
 }
