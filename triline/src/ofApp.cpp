af286fac39b19f77d6023763f57cc2d411a41403
dhayashi <eb37752@jp.ibm.com>
2014-10-16 17:36:53 +0900

    add valiable mode delete only tryangle method

--- triline/src/ofApp.cpp
+++ triline/src/ofApp.cpp
@@ -1,124 +1,38 @@
 ﻿#include "ofApp.h"
 
-int diff_r;
+int g_count;
+float g_scale_start;
+float g_scale_end;
+float g_r_poly;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofBackground(0);
     ofRectMode(OF_RECTMODE_CENTER);
     ofSetCircleResolution(64);
-
-    diff_r=0;
+    glLineWidth(3.0);
+    
+    g_count       = 8;
+    g_r_poly      = 100;
+    g_scale_start = 0;
+    g_scale_end   = 1;
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
-    diff_r = ofGetElapsedTimef() * 10;
-    diff_r%=200;
-}
-void draw_bow_triange(){
-
-    ofVec2f vp0;
-    ofVec2f vp1,vp2,vp3;
-    ofVec2f vp1s,vp1e;
-    ofVec2f vp2s,vp2e;
-    ofVec2f vp3s,vp3e;
-    ofVec2f vpn12,vpn23,vpn31;
-    ofVec2f vpmid12,vpmid23,vpmid31;
-    int     dist12,dist23,dist31;
-    float   deg12,deg23,deg31;
-    int r_tri;
-    float deg_tri;
-    float x,y;
-
-    ofVec2f vp[4];
-    ofVec2f vps[4],vpe[4];
-    ofVec2f vpn[4],vpmid[4];
-    float dist[4],deg[4];
-
-    float deg_;
-
-    vp0.set(0,0);
-    r_tri   = 100;
-    deg_tri = -90;
-
-    //各頂点(基準点)計算
-    x = r_tri * cos(ofDegToRad(deg_tri));
-    y = r_tri * sin(ofDegToRad(deg_tri));
-    vp1.set(x,y);
-
-    x = r_tri * cos(ofDegToRad(deg_tri+120));
-    y = r_tri * sin(ofDegToRad(deg_tri+120));
-    vp2.set(x,y);
-
-    x = r_tri * cos(ofDegToRad(deg_tri-120));
-    y = r_tri * sin(ofDegToRad(deg_tri-120));
-    vp3.set(x,y);
-
-    //２点間距離
-    dist12 = vp1.distance(vp2);
-    dist23 = vp2.distance(vp3);
-    dist31 = vp3.distance(vp1);
-
-    //２点間中間点
-    vpmid12 = vp1.getMiddle(vp2);
-    vpmid23 = vp2.getMiddle(vp3);
-    vpmid31 = vp3.getMiddle(vp1);
-
-    //２点間角度
-    deg12 = ofRadToDeg(atan2(vp2.y - vp1.y,vp2.x - vp1.x));
-    deg23 = ofRadToDeg(atan2(vp3.y - vp2.y,vp3.x - vp2.x));
-    deg31 = ofRadToDeg(atan2(vp1.y - vp3.y,vp1.x - vp3.x));
-
-    //２点間ベクトル算出
-    x = cos(ofDegToRad(deg12));
-    y = sin(ofDegToRad(deg12));
-    vpn12.set(x,y);
-
-    x = cos(ofDegToRad(deg23));
-    y = sin(ofDegToRad(deg23));
-    vpn23.set(x,y);
-
-    x = cos(ofDegToRad(deg31));
-    y = sin(ofDegToRad(deg31));
-    vpn31.set(x,y);
-
-    vp1s = vpmid12 - (vpn12 * diff_r);
-    vp1e = vpmid12 + (vpn12 * diff_r);
-    vp2s = vpmid23 - (vpn23 * diff_r);
-    vp2e = vpmid23 + (vpn23 * diff_r);
-    vp3s = vpmid31 - (vpn31 * diff_r);
-    vp3e = vpmid31 + (vpn31 * diff_r);
-
-    ofDrawBitmapString("vp1s",vp1s);
-    ofDrawBitmapString("vp1e",vp1e);
-    ofDrawBitmapString("vp2s",vp2s);
-    ofDrawBitmapString("vp2e",vp2e);
-    ofDrawBitmapString("vp3s",vp3s);
-    ofDrawBitmapString("vp3e",vp3e);
-
-    ofNoFill();
-    ofCircle(vp0,10);
-    ofCircle(vp0,r_tri);
-
-    ofLine(vp1s,vp1e);
-    ofLine(vp2s,vp2e);
-    ofLine(vp3s,vp3e);
-
 }
 
-void draw_bow_polygon(int poly){
+void draw_burr_polygon(int poly,float r_poly,float s_scale,float e_scale){
 
-#define POLY_MAX 13
+#define POLY_MAX 37
 
     float x,y;
     ofVec2f vp[POLY_MAX];
     ofVec2f vps[POLY_MAX],vpe[POLY_MAX];
     ofVec2f vpnormal[POLY_MAX],vpmiddle[POLY_MAX];
     float deg[POLY_MAX],dist[POLY_MAX];
-    float r_poly;
     int deg_split;
 
     r_poly = 100;
@@ -129,8 +43,8 @@ void draw_bow_polygon(int poly){
     for (int ipoly = 1; ipoly <= poly; ipoly++)
     {
         //基準頂点算出
-        x = r_poly * cos(ofDegToRad((ipoly -1) * deg_split));
-        y = r_poly * sin(ofDegToRad((ipoly -1) * deg_split));
+        x = r_poly * cos(ofDegToRad( (ipoly -1) * deg_split ));
+        y = r_poly * sin(ofDegToRad( (ipoly -1) * deg_split ));
         vp[ipoly].set(x,y);
     }
 
@@ -152,18 +66,18 @@ void draw_bow_polygon(int poly){
         vpnormal[i].set(x,y);
     }
 
+    //始点、終点距離の倍率を引数で指定
     for (int i = 1; i <= poly; i++)
     {
-        vps[i] = vp[i];
-        vpe[i] = vp[i] + (vpnormal[i] * 300);
+        vps[i] = vp[i] + (vpnormal[i] * (dist[i]*s_scale));
+        vpe[i] = vp[i] + (vpnormal[i] * (dist[i]*e_scale));
     }
 
     for (int i = 1; i <= poly; i++)
     {
         ofLine(vps[i],vpe[i]);
     }
-
-
+    
 }
 
 //--------------------------------------------------------------
@@ -172,15 +86,33 @@ void ofApp::draw(){
     ofPushMatrix();
     ofTranslate(ofGetWidth()/2,ofGetHeight()/2);
 
-    draw_bow_polygon(8);
+    //draw_burr_polygon(g_count,g_scale_start,g_scale_end);
+    draw_burr_polygon(g_count,0.1,0.9);
 
-    ofPopMatrix();
-
-
+    ofPopMatrix();    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    if (key==OF_KEY_UP)
+    {
+        g_count++;
+    }
+    if (key==OF_KEY_DOWN)
+    {
+        g_count--;
+    }
+    if (key==OF_KEY_RIGHT)
+    {
+        g_scale_end+=0.1;
+    }
+    if (key==OF_KEY_LEFT)
+    {
+        g_scale_end-=0.1;
+    }
+
+    g_count = ofClamp(g_count,3,36);
+
 
 }
 
