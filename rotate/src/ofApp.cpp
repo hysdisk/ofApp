af6b85d2204229d686a28607dffd84c1e3ce6e9e
dhayashi <eb37752@jp.ibm.com>
2014-11-26 18:40:29 +0900

    grid mesh draw trying... 	modified:   ofApp.cpp 	modified:   ofApp.h

--- rotate/src/ofApp.cpp
+++ rotate/src/ofApp.cpp
@@ -1,134 +1,215 @@
-#include "ofApp.h"
+﻿#include "ofApp.h"
+ofVboMesh mesh;
+int px;
+int py;
+int pz;
+int spacex;
+int spacey;
+int spacez;
+//--------------------------------------------------------------
 
-int end_deg;
-int circle_steps;
-ofColor kurveColor[9];
+void set_mesh_grid(){
 
+    mesh.clear();
+    mesh.setMode(OF_PRIMITIVE_POINTS);
+
+    //グリッド点作成
+    for (int iy = 0; iy < py; iy++) {
+        for (int ix = 0; ix < px; ix++) {
+            mesh.addVertex(ofVec3f(ix * spacex,iy * spacey, 0));   
+        }
+    }
+
+    //グリッド線作成
+    for (int iy = 0; iy < py; iy++) {
+        for (int ix = 0; ix < px; ix++) {
+
+            //横線
+            if (ix < px -1) {
+                mesh.addIndex(((iy*px) + ix));   
+                mesh.addIndex(((iy*px) + ix) + 1);   
+            }
+
+            //縦線
+            if (iy < py -1) {
+                mesh.addIndex(((iy*px) + ix));   
+                mesh.addIndex(((iy*px) + ix) + px);   
+            }
+        }
+    }
+}
+void set_mesh_grid_3d(){
+
+    mesh.clear();
+    mesh.setMode(OF_PRIMITIVE_LINES);
+
+    //グリッド点作成
+    for (int iz = 0; iz < pz; iz++) {
+        for (int iy = 0; iy < py; iy++) {
+            for (int ix = 0; ix < px; ix++) {
+                mesh.addVertex(ofVec3f(ix * spacex,iy * spacey, iz * spacez));   
+            }
+        }
+    }
+
+    //グリッド線作成
+    for (int iz = 0; iz < pz; iz++) {
+        for (int iy = 0; iy < py; iy++) {
+            for (int ix = 0; ix < px; ix++) {
+                //横線
+                if (ix < px -1) {
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix));   
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix) + 1);   
+                }
+
+                //縦線
+                if (iy < py -1) {
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix));   
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix) + px);   
+                }
+                //奥線
+                if (iz < pz -1) {
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix));   
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix) + (px * py));   
+                }
+            }
+        }
+    }
+
+    //x用yz面
+    //yz = (iy * py) + (iz * (py * pz)) 
+
+    //y用xz面
+    //xz = ix + (iz * px * pz)
+
+    //z用xy面
+    //xy = ix + (iy * px)
+
+
+}
+
+void update_mesh_grid_index_3d(){
+
+    mesh.clearIndices();
+
+    //グリッド線作成
+    for (int iz = 0; iz < pz; iz++) {
+        for (int iy = 0; iy < py; iy++) {
+            for (int ix = 0; ix < px; ix++) {
+                //横線
+                if (ix < px -1) {
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix));   
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix) + 1);   
+                }
+
+                //縦線
+                if (iy < py -1) {
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix));   
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix) + px);   
+                }
+                //奥線
+                if (iz < pz -1) {
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix));   
+                    mesh.addIndex(((iy * px) + ((px * py) * iz) + ix) + (px * py));   
+                }
+            }
+        }
+    }
+}
+
+
+void update_mesh_grid(float wave_deg){
+
+    ofPoint pos;
+
+    //グリッド点更新
+    for (int iy = 0; iy < py; iy++) {
+        for (int ix = 0; ix < px; ix++) {
+            pos.x = ix * spacex;
+            pos.y = iy * spacey;
+            pos.z = 30 * sin(ofDegToRad(ofMap(ix+iy,0,px+py,0,360)+wave_deg));
+
+            mesh.setVertex((iy*px) + ix,pos);
+        }
+    }
+}
 //--------------------------------------------------------------
 void ofApp::setup(){
 
-    ofBackground(ofColor::black);
-    end_deg = 180;
-    kurveColor[1] = ofColor(33,34,39);
-    kurveColor[2] = ofColor(48,45,72);
-    kurveColor[3] = ofColor(46,41,61);
-    kurveColor[4] = ofColor(61,44,78);
-    kurveColor[5] = ofColor(127,49,39);
-    kurveColor[6] = ofColor(142,49,34);
-    kurveColor[7] = ofColor(165,61,36);
-    kurveColor[8] = ofColor(193,106,63);
+    ofSetRectMode(OF_RECTMODE_CENTER);
 
-    circle_steps = 3;
+    ofBackground(ofColor::black);
+
+    w = ofGetWidth();
+    h = ofGetHeight();
+    cw = w / 2;
+    ch = h / 2;
+
+    sine_deg = 0;
+
+    px = 10;
+    py = 10;
+    pz = 10;
+    spacex = 50;
+    spacey = 50;
+    spacez = 50;
+
+    set_mesh_grid_3d();
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    //update_mesh_grid(ofGetElapsedTimef()*100);
+}
+
+void draw_sine_wave(float start_deg,float wave_deg){
+
+    int wave_length = 300;
+    int wave_height = 50;
+
+    ofPoint dpos;
+
+    for (int ideg = 0; ideg <= 360; ideg+=9)
+    {
+        dpos.x = ofMap(ideg,0,360,10,wave_length);
+        dpos.y = 0;
+        dpos.z = wave_height * sin(start_deg + (ofDegToRad(ideg)));
+
+        //dpos.rotate(wave_deg,ofVec3f(0, 0, 1));
+
+        ofNoFill();
+        //ofCircle(dpos,3);
+        ofRect(dpos,5,5);
+    }
 
 }
+
+
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 
-    ofTranslate(ofGetWidth()/2 ,ofGetHeight()/2);
-
-    ofMesh mesh;
-
-    ofVec3f pos_in;
-    ofVec3f pos_out;
-    ofVec3f r_pos;
-
-    float posx,posy;
-
-    int r_width;
-    int r_radius;
-    int r_count;
-    int r_deg;
-    int _rad;
-
-    r_count = 1;
-    r_width = 20;
-    r_radius = 20;
-
-    mesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
-    //mesh.setMode(OF_PRIMITIVE_POINTS);
-
-    mesh.addVertex(ofVec3f(0,0,0));
-
-    glPointSize(3.0);
-
-    for (int icount = 1; icount <= 8; icount++)
-    {
-        for (int ir = 0; ir <= end_deg / circle_steps; ir++)
-        {
-            _rad = r_radius + (r_width * icount);
-            posx = _rad * cos(ofDegToRad(ir*circle_steps));
-            posy = _rad * sin(ofDegToRad(ir*circle_steps));
-
-            pos_in.set(posx,posy,0);
-            mesh.addColor(kurveColor[icount]);
-            mesh.addVertex(pos_in);
-
-            _rad+=r_width;
-            posx = _rad * cos(ofDegToRad(ir*circle_steps));
-            posy = _rad * sin(ofDegToRad(ir*circle_steps));
-
-            pos_out.set(posx,posy,0);
-            mesh.addColor(kurveColor[icount]);
-            mesh.addVertex(pos_out);
-
-            r_deg = ir*circle_steps;
-
-        }
+    cam.begin();
+    ofPushMatrix();{
+        //ofTranslate(cw,ch,0);
+        //ofRotateY(ofGetElapsedTimef() * 10 );
+        //for (int i = 0; i <= 360; i+=5){
+        //    ofRotateZ(i);
+        //    draw_sine_wave(ofGetElapsedTimef(),i);
+        //}
         mesh.draw();
-        mesh.clear();
+    }ofPopMatrix();
 
-        _rad = (r_radius + r_width * 11);
-        posx = _rad * cos(ofDegToRad(r_deg));
-        posy = _rad * sin(ofDegToRad(r_deg));
-        r_pos.set(posx,posy);
+    cam.end();
 
-        for (int ir = r_deg-180; ir >= -r_deg; ir--)
-        {
-            //
-            _rad = r_radius + (-(icount - 9)) * r_width;
-            posx = _rad * cos(ofDegToRad(ir));
-            posy = _rad * sin(ofDegToRad(ir));
+    ofDrawBitmapStringHighlight(ofToString(ofGetElapsedTimef()),10,10);
 
-            pos_in.set(posx,posy,0);
-            mesh.addColor(kurveColor[icount]);
-            mesh.addVertex(pos_in);
-
-            _rad+=r_width;
-            posx = _rad * cos(ofDegToRad(ir));
-            posy = _rad * sin(ofDegToRad(ir));
-
-            pos_out.set(posx,posy,0);
-            mesh.addColor(kurveColor[icount]);
-            mesh.addVertex(pos_out);
-        }
-
-        ofPushMatrix();
-        ofTranslate(r_pos);
-        mesh.draw();
-        mesh.clear();
-        ofPopMatrix();
-    }
-    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
-    if (key==OF_KEY_RIGHT)
-    {
-        end_deg++;
-    }
-
-    if (key==OF_KEY_LEFT)
-    {
-        end_deg--;
-    }
 
 }
 
